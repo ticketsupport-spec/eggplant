f9c653aea244274e24b8260796a231099f0e3c7e
ticketsupport-spec <ticketsupport@midlandauctions.ca>
2026-03-18 16:08:41 -0400

    Add full Eggplant WordPress scaffold (multi-file, no Composer)

---
+++ eggplant/includes/class-eggplant.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * The file that defines the core plugin class
+ *
+ * This is used to define the class that will be created 
+ * based on the structure of the plugins for its functionality.
+ *
+ * @link https://developer.wordpress.org/plugins/the-basics/
+ * @since 1.0.0
+ * @package Eggplant
+ */
+
+class Eggplant {
+    // Add your core plugin functionality here
+}
